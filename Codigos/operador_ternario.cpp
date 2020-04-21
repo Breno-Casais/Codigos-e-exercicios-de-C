82f7d11636d9215f68e7c5e8a8d45bf336a20c38
Breno-Casais <63324547+Breno-Casais@users.noreply.github.com>
2020-04-21 07:54:00 -0300

    Operadores ternarios

---
+++ Codigos/operador_ternario.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num = 10;
+	
+	/*if (num > 10)
+		cout << "numero maior que 10";
+	else
+		cout << "numero menor que 10";
+	*/
+	
+	num > 10 ? cout << "maior" : cout << "menor";
+	
+return 0;
+}
