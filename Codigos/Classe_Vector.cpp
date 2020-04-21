c37dabd15ced2aa2954e4ce8df0f6cbc20623e1f
Breno-Casais <63324547+Breno-Casais@users.noreply.github.com>
2020-04-21 16:25:19 -0300

    Create Classe_Vector.cpp

---
+++ Codigos/Classe_Vector.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	vector <int> v;
+	v.push_back(20); // coloca o elemento no final 
+	v.push_back(30); 
+	v.push_back(40);
+	
+	int tam = v.size();
+	
+	for (int i = 0; i < tam; i++)
+	{
+		cout << v[i] << " " << endl;
+	}
+	
+	vector<int>::iterator it = v.begin();
+return 0;
+}
