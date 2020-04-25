1ac9d729a4585946d90e019e9df50715bd1d4891
Breno-Casais <63324547+Breno-Casais@users.noreply.github.com>
2020-04-25 14:29:50 -0300

    criando uma namespace

--- Codigos/criandoNamespace.cpp
+++ Codigos/criandoNamespace.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+namespace ns1{
+	int num = 42;
+	
+	class A{
+		public :
+			void imprimir(){
+				cout << "ola, sou a class A do namespace ns1"<<endl;
+			}
+	};
+}
+
+namespace ns2{
+	int num = 50;
+	class A{
+		public :
+			void imprimir(){
+				cout << "ola, sou a class A do namespace ns2"<<endl;
+			}
+	};
+}
+
+int main(){
+	ns1::A ns1A;
+	ns2::A ns2A;
+
+	ns1A.imprimir();
+	ns2A.imprimir();
+	return 0;
+}
+
